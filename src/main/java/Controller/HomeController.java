ef6a064cc233d38ad489d06ffaf37d5f1c63de8e
onehowon <flash0221@naver.com>
2024-05-07 00:52:44 +0900

    test

---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+
+        return "home";
+    }
+}
